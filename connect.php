4e5afc48e121a7cb8ab7b4fc1af91141ecbb7a6f
rianenichelle <imrianenichelle@gmail.com>
2020-11-30 01:01:53 +0800

    added db connection

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
-    $servername = "localhost";
-    $username="root";
-    $password="";
-    $dbname="ismis";
+    $servername = "sql12.freemysqlhosting.net";
+    $username="sql12379133";
+    $password="uv4SliTfGV";
+    $dbname="sql12379133";
 ?>
--- index.php
+++ index.php
@@ -74,7 +74,7 @@
 		   $password = $_POST['password'];
 
 		
-		   $conn = new mysqli($servername,$username,$password,$dbname);
+		   $conn = new mysqli($servername, $username, $password, $dbname);
            
 	       if($conn->connect_error) {
               die("Connection failed: " . $conn->connect_error);
