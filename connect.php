91a35e31583cbaa1c04661b8dffd374f5e9a7681
rianenichelle <imrianenichelle@gmail.com>
2020-11-30 01:24:13 +0800

    when you are young they assume you know nothing

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
     $servername = "sql12.freemysqlhosting.net";
-    $username="sql12379133@ec2-52-8-112-233.us-west-1.compute.amazonaws.com";
+    $username="sql12379133";
     $password="uv4SliTfGV";
     $dbname="sql12379133";
 ?>
