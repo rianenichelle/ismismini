5aa7e1a670ca9b718cbabe490cb2b07f1a22ce44
rianenichelle <imrianenichelle@gmail.com>
2020-11-30 01:10:33 +0800

    updated db connection

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
     $servername = "sql12.freemysqlhosting.net";
-    $username="sql12379133";
+    $username="sql12379133@ec2-52-8-112-233.us-west-1.compute.amazonaws.com";
     $password="uv4SliTfGV";
     $dbname="sql12379133";
 ?>
